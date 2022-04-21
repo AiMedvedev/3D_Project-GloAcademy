44d777b97ba91a90efbd98ffaef40a370f4c4aba
Alex <forcablaugrana@gmail.com>
2022-04-22 00:39:56 +0300

    21lesson almost done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import smoothScroll from './modules/scroll';
+import calculator from './modules/calculator';
 
 timer('25 april 2022');
 menu();
 modal();
 smoothScroll();
+calculator();
+
---
+++ src/modules/calculator.js
@@ -0,0 +1,61 @@
+const calculator = () => {
+
+    const calcInputs = document.querySelectorAll('input.calc-item');
+    const textInputs = document.querySelectorAll('input[Placeholder="Ваше имя"]');
+    const textArea = document.querySelector('input[Placeholder="Ваше сообщение"]');
+    const emailInputs = document.querySelectorAll('input[Type="email"]');
+    const telInputs = document.querySelectorAll('input[Type="tel"]');
+
+    
+    // 1.
+    calcInputs.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/\D+/g, "");
+        })
+    });
+    // 2.1.
+    textInputs.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Я -]/g, "");
+        })
+    });
+
+    textArea.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я -]/g, "");
+    });
+    // 2.2.
+    emailInputs.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9a-zA-Z@-_.!~*']/g, "");
+        })
+    });
+    // 2.3.
+    telInputs.forEach(item => {
+        item.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9()-]/g, "");
+        })
+    });
+
+    // 3.2.
+
+    textInputs.forEach(item => {
+        item.onblur = function(e) {
+            e.target.value = e.target.value.replace(/-{2,}/g, "-");
+            e.target.value = e.target.value.replace(/ {2,}/g, " ");
+            e.target.value = e.target.value.replace(/^[ -]*/g, "");
+            e.target.value = e.target.value.replace(/[а-я]?[ -]$/g, "");
+            //e.target.value = e.target.value.replace(/^ /g, "");
+            //e.target.value = e.target.value.replace(/ $/g, "");
+            
+
+            //e.target.value = e.target.value.replace(/ {2,}/g, " ");
+            /* if (!input.value.includes('@')) { // не email
+            input.classList.add('invalid');
+            error.innerHTML = 'Пожалуйста, введите правильный email.'
+            } */
+        };
+    });
+}
+
+export default calculator;
+
