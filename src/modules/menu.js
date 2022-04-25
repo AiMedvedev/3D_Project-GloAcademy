98026ddb1cf0245ffa8978fb52805f15ef4f42c5
Alex <forcablaugrana@gmail.com>
2022-04-25 13:12:06 +0300

    fixes made

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -10,7 +10,7 @@ const menu = () => {
 
     menuBtn.addEventListener('click', handleMenu);
     closeBtn.addEventListener('click', handleMenu);
-
+    
     menu.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
